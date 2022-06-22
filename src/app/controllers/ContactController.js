18bad6e90f05854bc763b83abe7a999687bea829
Nicolas Oliveira <nicksouza861@gmail.com>
2022-06-21 21:16:52 -0300

    feat: creates remaining repositories

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -8,16 +8,105 @@ class ContactController {
     response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // List ONE registry
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not Found
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
-  update() {
+  async store(request, response) {
+    // Create a registry
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response
+        .status(400)
+        .json({ error: 'Name field is required!' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already being used!' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
+  }
+
+  async update(request, response) {
     // Edit a registry
+    const { id } = request.params;
+    const {
+      name,
+      email,
+      phone,
+      category_id,
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found!' });
+    }
+
+    if (!name) {
+      return response
+        .status(400)
+        .json({ error: 'Name field is required!' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: 'This e-mail is already being used!' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
   }
 
-  delete() {
+  async delete(request, response) {
     // Delete a registry
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      // 404: Not Found
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    // 204: No Content
+    response.sendStatus(204);
   }
 }
 
