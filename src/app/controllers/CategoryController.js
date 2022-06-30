e114df3ffdbabe978a50e3048cedbfa39a5fd005
Nicolas Oliveira <nicksouza861@gmail.com>
2022-06-29 21:28:26 -0300

    feat: creates categories classes

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+
+    response.send(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name field is required!' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.send(category);
+  }
+}
+
+module.exports = new CategoryController();
