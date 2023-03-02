f6c9dc3ef8fcf692ace15efe3797c42b81cfad66
Nicolas Oliveira <nicksouza861@gmail.com>
2023-03-01 21:52:51 -0300

    feat: creates contact card

---
+++ web/src/components/ContactsList/styles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 32px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  strong {
+    font-size: 24px;
+  }
+
+  a {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    padding: 8px 16px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin: 24px 0 0;
+
+  header {
+    margin-bottom: 8px;
+
+    .sort-button {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+      border: none;
+      background: transparent;
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        padding: 4px;
+        border-radius: 4px;
+        margin-left: 8px;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: 8px;
+    }
+  }
+`;
--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
