9db36be468cbb7da8f47186b387857792eb02409
Nicolas Oliveira <nicksouza861@gmail.com>
2023-03-06 22:05:48 -0300

    feat: creates pageHeader

---
+++ web/src/components/PageHeader/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  a {
+    text-decoration: none;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.colors.primary.main};
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin-bottom: 8px;
+
+    img {
+      transform: rotate(-90deg);
+    }
+  }
+
+  h1 {
+    font-size: 24px;
+    line-height: 30px;
+  }
+`;
